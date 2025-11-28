23acd3195854f75c2557e7540abd2f22ed8d6c44
mdumar0001 <mdumar84002024@gmail.com>
2025-11-28 09:38:15 +0530

    Update in db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,37 +1,37 @@
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-// dotenv.config();
-
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URI, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//         });
-//         console.log('MongoDB connected');
-//     } catch (error) {
-//         console.error('MongoDB connection failed:', error.message);
-//         process.exit(1);
-//     }
-// };
-
-// module.exports = connectDB;
-
-import mongoose from "mongoose";
+dotenv.config();
 
 const connectDB = async () => {
-  try {
-    mongoose.connection.on("connected", () =>
-      console.log("DATABASE CONNECTED")
-    );
-    await mongoose.connect(`${process.env.MONGO_URI}/test`);
-  } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1);
-  }
-
-  //it will create a database prescripto in mongodb
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/test`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error('MongoDB connection failed:', error.message);
+        process.exit(1);
+    }
 };
 
-export default connectDB;
+module.exports = connectDB;
+
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     mongoose.connection.on("connected", () =>
+//       console.log("DATABASE CONNECTED")
+//     );
+//     await mongoose.connect(`${process.env.MONGO_URI}/test`);
+//   } catch (error) {
+//     console.error("MongoDB connection failed:", error.message);
+//     process.exit(1);
+//   }
+
+//   //it will create a database prescripto in mongodb
+// };
+
+// export default connectDB;
