bcbb098ebc5b75a185a23f1f1308bb628f7739ab
mdumar0001 <mdumar84002024@gmail.com>
2025-11-28 12:56:10 +0530

    Update new changes db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGO_URI}/test`, {
+        await mongoose.connect(`${process.env.MONGO_URI}`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
